fed282b7f4a661fad018b3cff3816d0500f77acb
SovietSpy2 <sovietspy2@yandex.com>
2018-11-22 09:38:57 +0100

    start

---
+++ MapDetails.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="MapDetails" tilewidth="32" tileheight="32" tilecount="225" columns="15">
+ <image source="tile_castle.png" width="480" height="480"/>
+</tileset>
